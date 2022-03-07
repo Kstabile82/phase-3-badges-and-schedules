506ec8d7e9ac3597a41b1da07257d50f0df7d783
KarinaStabile <karina.stabile@gmail.com>
2022-03-06 21:08:06 -0500

    commit tree 438d8bf3453b894650f6c2a70311e76572b73f12

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(list_of_speakers)
+    list_of_speakers.map.with_index(1) do |speaker, room| 
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+ def printer(names)
+   batch_badge_creator(names).each do |badge|
+    puts badge
+   end
+   assign_rooms(names).map do |idx|
+    puts idx
+   end
+ end
+
+
